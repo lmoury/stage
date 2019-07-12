6a4065aa4dab8c59d7fdecc225404335f96d00ee
lmoury <laurent.moury@hotmail.be>
2019-07-13 00:44:49 +0200

    avant vacance

--- src/Controller/Frontend/ParkingController.php
+++ src/Controller/Frontend/ParkingController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Frontend;
 use App\Entity\Operation;
 use App\Repository\ParkingRepository;
 use App\Form\OperationParkingType;
+use App\Form\OperationParkingsType;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,12 +37,25 @@ class ParkingController extends AbstractController
      * @Route("parking", name="parking")
      * @param ParkingRepository $repository
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        $form = $this->createForm(OperationParkingsType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $operation->setOperation(4);
+            $operation->setQuai(NULL);
+            $operation->setDateCreation(new \DateTime());
+            $this->em->flush();
+            $this->addFlash('success', 'La remorque <strong>'.$operation->getRemorque()->getRemorque().'</strong> à été mise sur le parking : <strong>'.$operation->getParking()->getDenomination().'</strong>');
+            return $this->redirectToRoute('parking');
+        }
+
         $parkings = $this->repository->getParkings();
         return $this->render('frontend/parking/index.html.twig', [
             'current_url' => $this->current_url,
             'parkings' => $parkings,
+            'form' => $form->createView(),
         ]);
     }
 
--- src/Form/OperationParkingsType.php
+++ src/Form/OperationParkingsType.php
@@ -3,20 +3,31 @@
 namespace App\Form;
 
 use App\Entity\Operation;
+use App\Entity\Remorque;
+use App\Entity\Parking;
+use App\Repository\RemorqueRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class OperationParkingsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('operation')
-            ->add('date_creation')
-            ->add('remorque')
-            ->add('parking')
-            ->add('quai')
+            ->add('remorque', EntityType::class, [
+                'class' => Remorque::class,
+                'choice_label' => 'remorque',
+                'query_builder' => function (RemorqueRepository $qr) {
+                    return $qr->createQueryBuilder('q')
+                    ->andWhere('q.maintenance = false');
+                }
+            ])
+            ->add('parking', EntityType::class, [
+                'class' => Parking::class,
+                'choice_label' => 'denomination',
+            ])
         ;
     }
 
